dfdb0389de03e7cfa717e61d1eb7faf36c841d85
sakai <endeavor2wako@gmail.com>
2024-05-04 11:02:01 +0900

    アラーム機能とスリープタイマー機能を追加。再生のオンオフをmpvの動作状況を取得してから切り替える方法に変更。

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 	"sync"
 	"math"
+	"encoding/json"
 	//~ "github.com/mattn/go-tty"
 	"github.com/davecheney/i2c"
 	"github.com/stianeikeland/go-rpio/v4"
@@ -57,9 +58,17 @@ type StationInfo struct {
 	url string
 }
 
+type mpvIRC struct {
+    Data       	interface{}	 `json:"data"`
+	Request_id  *int	 `json:"request_id"`
+    Err 		string	 `json:"error"`
+    Event		string	 `json:"event"`
+}
+
 var (
 	mpv	net.Conn
 	oled aqm1602y.AQM1602Y
+	mu sync.Mutex
 	stlist []*StationInfo
 	colon uint8
 	pos int
@@ -68,13 +77,14 @@ var (
 	volume int
 	re_table = []int8{0,1,-1,0,-1,0,0,1,1,0,0,-1,0,-1,1,0}
 	re_count int = 0
+	display_colon = []uint8{' ',':'}
+	display_sleep = []uint8{' ',' ','S'}
 	clock_mode uint8
 	alarm_time time.Time
+	tuneoff_time time.Time
 	alarm_set_mode bool
 	alarm_set_pos int
-
 )
-
 
 func setup_station_list () {
 	file, err := os.Open(stationlist)
@@ -89,19 +99,20 @@ func setup_station_list () {
 	name := ""
 	for scanner.Scan() {
 		s = scanner.Text()
+		if strings.Contains(s, "#EXTINF:") == true {
+			f = true
+			_, name, _ = strings.Cut(s, "/")
+			name = strings.Trim(name, " ")
+			continue
+		}
 		if f {
 			if len(s) != 0 {
 				f = false
 				stmp := new(StationInfo)
 				stmp.url = s
-				stmp.name = string(name+"                ")[:15]
+				stmp.name = string(name+"                ")[:16]
 				stlist = append(stlist, stmp)
 			}
-		}
-		if strings.Contains(s, "#EXTINF:") == true {
-			f = true
-			_, name, _ = strings.Cut(s, "/")
-			name = strings.Trim(name, " ")
 		}
 	}
 }
@@ -120,6 +131,45 @@ func mpv_send(s string) {
 	}
 }
 
+func mpv_playing_now() bool {
+	var res []mpvIRC
+	rv := false
+	//~ mpv.Write([]byte("{\"command\": [\"get_property\",\"filename\"]}\x0a"))
+	mpv.Write([]byte("{\"command\": [\"get_property\",\"playlist\"]}\x0a"))
+	for {
+		n, err := mpv.Read(readbuf)
+		if err != nil {
+			mes := "mpv conn error."
+			infoupdate(0, &mes)
+			mes = "HUP"
+			infoupdate(1, &mes)
+			log.Fatal(err)	
+		}
+
+exit_this:
+		for _, elem := range strings.Split(string(readbuf[:n]),"\n") {
+			if err := json.Unmarshal([]byte("[ "+elem+" ]"), &res); err != nil {
+				continue
+			}
+			for _, r := range res {
+				if r.Err == "property unavailable" {
+					continue
+				}
+				if r.Err == "success" && r.Request_id != nil && r.Data != nil {
+					//~ fmt.Println(r.Data)
+					rv = true
+					break exit_this
+				}
+			}
+		}
+		
+		if n < 1024 {
+			break
+		}
+	}
+	return rv
+}
+
 func mpv_setvol(vol int) int {
 	var v int
 	if vol < 1 {
@@ -132,12 +182,6 @@ func mpv_setvol(vol int) int {
 	return v
 }
 
-func beep() {
-	s := fmt.Sprintf("{\"command\": [\"loadfile\",\"%s\"]}\x0a", "/usr/local/share/mpvradio/sounds/button57.mp3")
-	mpv_send(s)
-}
-
-var mu sync.Mutex
 func infoupdate(line uint8, mes *string) {
 	mu.Lock()
 	defer mu.Unlock()
@@ -226,19 +270,26 @@ func btninput(code chan<- ButtonCode) {
 }
 
 func tune() {
+	infoupdate(0, &stlist[pos].name)
+	
 	args := strings.Split(stlist[pos].url, "/")
 	if args[0] == "plugin:" {
 		cmd := exec.Command("/usr/local/share/mpvradio/plugins/"+args[1], args[2])
 		err := cmd.Run()
 		if err != nil {
-			stmp := "Tuning Error"
+			stmp := "Tuning Error    "
 			infoupdate(0, &stmp)
 		}
 	} else {
 		s := fmt.Sprintf("{\"command\": [\"loadfile\",\"%s\"]}\x0a", stlist[pos].url)
 		mpv_send(s)
 	}
-	
+}
+
+func radio_stop() {
+	mpv_send("{\"command\": [\"stop\"]}\x0a")
+	stmp := "                "
+	infoupdate(0, &stmp)
 }
 
 func alarm_time_inc () {
@@ -249,12 +300,24 @@ func alarm_time_inc () {
 		// minute
 		alarm_time = alarm_time.Add(1*time.Minute)
 	}
+	alarm_time = time.Date(2009, 1, 1, alarm_time.Hour(), alarm_time.Minute(), 0, 0, time.UTC)
+}
+
+func alarm_time_dec () {
+	if alarm_set_pos == 1 {
+		// minute
+		// 時間が進んでしまうのでhourも補正する
+		alarm_time = alarm_time.Add(59*time.Minute)
+	}
+	// hour
+	alarm_time = alarm_time.Add(23*time.Hour)
+	alarm_time = time.Date(2009, 1, 1, alarm_time.Hour(), alarm_time.Minute(), 0, 0, time.UTC)
 }
 
 func showclock() {
-	var s, s0,s1,s2 string
-	
 	mu.Lock()
+	defer mu.Unlock()
+	var s0 string
 	// alarm
 	if clock_mode & 1 == 1 {
 		if alarm_set_mode && colon == 1 {
@@ -268,25 +331,17 @@ func showclock() {
 			} 
 		} else {
 			s0 = fmt.Sprintf("A%02d:%02d", alarm_time.Hour(),
-										alarm_time.Minute())
+											alarm_time.Minute())
 		}
 	} else {
 		s0 = "      "
 	}
 	
-	// sleep
-	if clock_mode & 2 == 2 {
-		s1 = "S"
-	} else {
-		s1 = " " 
-	}
-	
-	s2 = fmt.Sprintf("%02d%c%02d", time.Now().Hour(),
-									[]uint8{' ',':'}[colon],
+	s2 := fmt.Sprintf("%02d%c%02d", time.Now().Hour(),
+									display_colon[colon],
 										time.Now().Minute())
-	s = fmt.Sprintf("%s %s   %s", s0, s1, s2)
+	s := fmt.Sprintf("%s %c   %s", s0, display_sleep[clock_mode & 2], s2)
 	oled.PrintWithPos(0,1,[]byte(s))
-	mu.Unlock()
 }
 
 func main() {
@@ -300,7 +355,9 @@ func main() {
 	oled.Configure()
 	oled.PrintWithPos(0, 0, []byte("radio"))
 
-	mpvprocess = exec.Command("/usr/bin/mpv", MPVOPTION1, MPVOPTION2, MPVOPTION3, MPVOPTION4, MPVOPTION5)
+	mpvprocess = exec.Command("/usr/bin/mpv", 	MPVOPTION1, MPVOPTION2, 
+												MPVOPTION3, MPVOPTION4, 
+												MPVOPTION5)
 	err = mpvprocess.Start()
 	if err != nil {
 		stmp := "mpv fault."
@@ -338,15 +395,38 @@ func main() {
 	clock_mode = clock_mode_normal
 	
 	select_btn_repeat_count := 0
+
 	mode_btn_repeat_count := 0
 	alarm_set_mode = false
 	alarm_set_pos = 0
-	alarm_time = time.Date(2009, 1, 1, 12, 0, 0, 0, time.UTC)
+	alarm_time = time.Unix(0, 0).UTC()
+	tuneoff_time = time.Unix(0, 0).UTC()
 	btncode := make(chan ButtonCode)
 	go btninput(btncode)
 	
 	for {
 		select {
+			default:
+				time.Sleep(10*time.Millisecond)
+				if alarm_set_mode == false {
+					if (clock_mode & clock_mode_alarm) != 0 {
+						// アラーム
+						if alarm_time.Hour() == time.Now().Hour() &&
+						   alarm_time.Minute() == time.Now().Minute() {
+							clock_mode ^= clock_mode_alarm
+							tune()
+						}
+					}
+					if (clock_mode & clock_mode_sleep) != 0 {
+						// スリープ
+						if tuneoff_time.Hour() == time.Now().Hour() &&
+						   tuneoff_time.Minute() == time.Now().Minute() {
+							clock_mode ^= clock_mode_sleep
+							radio_stop()
+						}
+					}
+				}
+				
 			case <-colonblink.C:
 				colon ^= 1
 				showclock()
@@ -364,33 +444,39 @@ func main() {
 							// 通常時はアラーム、スリープのオンオフを行う
 							clock_mode++
 							clock_mode &= 3
+							if (clock_mode & clock_mode_sleep) != 0 {
+								// スリープ時刻の設定を行う
+								tuneoff_time = time.Now().Add(30*time.Minute)
+							}
 						}
 						
 					case (btn_station_mode|btn_station_repeat):
 						// alarm set
 						mode_btn_repeat_count++
-						if mode_btn_repeat_count > 4 {
+						if mode_btn_repeat_count > 3 {
 							// アラーム時刻の設定へ
 							alarm_set_mode = true
 							alarm_set_pos = 0
 						}
 						
 					case (btn_station_select):
-						infoupdate(0, &stlist[pos].name)
-						tune()
+						if mpv_playing_now() == true {
+							//~ fmt.Println("true")
+							radio_stop()
+						} else {
+							//~ fmt.Println("false")
+							tune()
+						}
 					
 					case (btn_station_select|btn_station_repeat):
-						select_btn_repeat_count++
-						if select_btn_repeat_count > 4 {
-							mpv_send("{\"command\": [\"stop\"]}\x0a")
-							stmp := "                "
-							infoupdate(0, &stmp)
-						}
+						//~ select_btn_repeat_count++
+						//~ if select_btn_repeat_count > 3 {
+							//~ radio_stop()
+						//~ }
 						
 					case btn_station_repeat_end:
 						if alarm_set_mode == false {
 							if select_btn_repeat_count == 0 && mode_btn_repeat_count == 0 { 
-								infoupdate(0, &stlist[pos].name)
 								tune()
 							}
 						}
@@ -400,7 +486,9 @@ func main() {
 					case (btn_station_next|btn_station_repeat):
 						if alarm_set_mode {
 							// アラーム設定時は時刻設定を行う
+							// リピート時の表示が追いつかないのでここでも表示する
 							alarm_time_inc ()
+							showclock()
 						} else {
 							if pos < stlen -1 {
 								pos++
@@ -415,34 +503,41 @@ func main() {
 						} else {
 							if pos < stlen -1 {
 								pos++
-								infoupdate(0, &stlist[pos].name)
 								tune()
 							}
 						}
 						
 					case (btn_station_prior|btn_station_repeat):
-						if pos > 0 {
-							pos--
-							infoupdate(0, &stlist[pos].name)
+						if alarm_set_mode {
+							// アラーム設定時は時刻設定を行う
+							// リピート時の表示が追いつかないのでここでも表示する
+							alarm_time_dec ()
+							showclock()
+						} else {
+							if pos > 0 {
+								pos--
+								infoupdate(0, &stlist[pos].name)
+							}
 						}
-						
 					case btn_station_prior:
-						if pos > 0 {
-							pos--
-							infoupdate(0, &stlist[pos].name)
-							tune()
+						if alarm_set_mode {
+							// アラーム設定時は時刻設定を行う
+							alarm_time_dec ()
+						} else {
+							if pos > 0 {
+								pos--
+								tune()
+							}
 						}
 				}
 
-			default:
-				time.Sleep(10*time.Millisecond)
 		}
 	}
 	
 	// shutdown this program
 	err = mpvprocess.Process.Kill()
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 	oled.DisplayOff()
 }
